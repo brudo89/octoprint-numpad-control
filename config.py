3bc5b42f58ce3adf1ea30826c164cc9dc9dd00c5
brudo89 <54287877+brudo89@users.noreply.github.com>
2020-07-03 22:05:48 +0200

    adjusted keys

--- config.py
+++ config.py
@@ -1,6 +1,8 @@
-API_KEY = None
 
 LOGFILE = None
+TEST_MODE = None
+
+API_KEY = None
 OCTO_HOST = 'octopi'
 OCTO_PORT = 80
 
@@ -15,7 +17,7 @@ JOG_STEP_Z = 10
 BED_ON_TEMP = 60
 TOOL_ON_TEMP = 200
 
-# SCN key codes
+# SCN key codes for "Holtek Semiconductor, Inc. Keyboard"
 SCN_NUMS = (79, 80, 81,  # 1 2 3
             75, 76, 77,  # 4 5 6
             71, 72, 73)  # 7 8 9
@@ -25,5 +27,5 @@ SCN_DOT = 83
 SCN_ENTER = 28
 SCN_MINUS = 74
 SCN_PLUS = 78
-SCN_DIV = 53
+SCN_DIV = 98  # 53
 SCN_MULT = 55
